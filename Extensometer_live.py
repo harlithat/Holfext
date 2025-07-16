323e6a1eb597f52314e2d66ddc711794a5f57fc7
harlithat <harli@jaws.co.za>
2025-07-16 14:08:15 +0200

    Update Extensometer_live.py

--- Extensometer_live.py
+++ Extensometer_live.py
@@ -23,9 +23,9 @@ def check_password():
         else:
             st.success("Access granted ✅")
 
-if password == "LetMeIn123":
-    st.success("Access granted ✅")
-    st.session_state["password"] = ""
+        if password == "LetMeIn123":
+            st.success("Access granted ✅")
+            st.session_state["password"] = ""
 
 check_password()
 
